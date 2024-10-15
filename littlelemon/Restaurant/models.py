ff499fa9df498961dc8132ec63253bf00b0dae63
MichelleKalman <kalman.michele@gmail.com>
2024-10-15 09:40:05 +0300

    added models

--- littlelemon/Restaurant/models.py
+++ littlelemon/Restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField()
+    
+    def __str__(self):
+        return f"Booking for {self.no_of_guests} by {self.name} on {self.booking_date}"
+    
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return f"{self.title}"
